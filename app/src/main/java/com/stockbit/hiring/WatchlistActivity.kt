7850cd71eea843fd23c82b8720488bc2f5a8a7af
Oktavia Pretty Amalia <prettyamalia12@gmail.com>
2022-01-23 21:41:34 +0800

    Set Pull to refresh on watchlist page

--- app/src/main/java/com/stockbit/hiring/WatchlistActivity.kt
+++ app/src/main/java/com/stockbit/hiring/WatchlistActivity.kt
@@ -1,9 +1,14 @@
 package com.stockbit.hiring
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.stockbit.hiring.adapter.WatchlistAdapter
 import com.stockbit.hiring.di.APIService
 import com.stockbit.hiring.di.ServiceBuilder
@@ -15,14 +20,39 @@ import retrofit2.Response
 class WatchlistActivity : AppCompatActivity() {
 
     lateinit var rvWatchlist : RecyclerView
+    lateinit var swipeRefresh : SwipeRefreshLayout
+
+    private val refreshListener = SwipeRefreshLayout.OnRefreshListener {
+        swipeRefresh.isRefreshing = true
+        // call api to reload the screen
+        getWatchlist()
+        //set the refresh animation to false again
+        swipeRefresh.isRefreshing = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_watchlist)
 
         rvWatchlist = findViewById(R.id.rvWatchlist)
+        swipeRefresh = findViewById(R.id.swipe_container)
+        swipeRefresh.setOnRefreshListener(refreshListener);
 
+        setHeaderLayout()
         getWatchlist()
+
+    }
+
+    private fun setHeaderLayout() {
+        findViewById<ImageView>(R.id.imgLeftHeader)
+            .setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_list))
+
+        findViewById<ImageView>(R.id.imgRightHeader)
+            .setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_paper_note))
+
+//        hide title and show the logo
+        findViewById<TextView>(R.id.txtHeader).visibility = View.GONE
+        findViewById<ImageView>(R.id.imgLogo).visibility = View.VISIBLE
     }
 
     private fun getWatchlist(){
@@ -34,14 +64,13 @@ class WatchlistActivity : AppCompatActivity() {
             override fun onResponse(call: Call<Object>, response: Response<Object>) {
                 if (response.isSuccessful) {
                     rvWatchlist.apply {
-                        if (response.body()!=null){
+                        if (response.body() != null){
                             setHasFixedSize(true)
                             layoutManager = GridLayoutManager(this@WatchlistActivity, 1)
                             adapter = WatchlistAdapter(response.body()!!)
                         }else{
                             setErrorMessage()
                         }
-
                     }
                 } else{
                         setErrorMessage()
